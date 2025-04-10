3c91eb4125954e14cc0bd103e54be82f0ab11d6d
LocalHostDiluk <169072534+LocalHostDiluk@users.noreply.github.com>
2025-04-10 02:21:49 -0600

    Update pedidoRoutes.js

--- src/routes/pedidoRoutes.js
+++ src/routes/pedidoRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { crearPedido, obtenerPedidos, obtenerPedidoPorId, actualizarEstadoPedido, cancelarPedido  } from "../controller/pedidoController.js";
+import { crearPedido, obtenerPedidos, obtenerPedidoPorId, actualizarEstadoPedido, cancelarPedido, obtenerPedidosPorCliente  } from "../controller/pedidoController.js";
 
 const router = express.Router();
 
@@ -7,7 +7,7 @@ router.post("/crear", crearPedido);
 router.get('/all', obtenerPedidos); 
 router.get('/pedido/:id', obtenerPedidoPorId); 
 router.delete('/cancelar/:id', cancelarPedido); 
-router.patch('/actualizar/:id', actualizarEstadoPedido); 
-
+router.patch('/actualizar/:id', actualizarEstadoPedido);
+router.get('/pedido/cliente/:clienteId', obtenerPedidosPorCliente); // Define la nueva ruta
 
 export default router;
